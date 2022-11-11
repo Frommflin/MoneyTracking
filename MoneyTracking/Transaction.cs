5ea7864818b3f608bbf9a1f6aafea9ba1567f0c4
Frommflin <freakyfrommelin@outlook.com>
2022-11-11 13:57:54 +0100

    Added class to handle transactions

---
+++ MoneyTracking/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracking
+{
+    internal class Transaction
+    {
+        public string TransactionId { get; }
+        public string TransactionType { get; set; }
+        public string TransactionTitle { get; set; }
+        public int TransactionAmount { get; set; }
+        public DateTime Date { get; set; }
+
+        private static int TransactionNumberSeed = 234987;
+
+        public Transaction(string transactionId, string transactionType, string transactionTitle, int transactionAmount, DateTime date)
+        {
+            TransactionId = transactionId;
+            TransactionType = transactionType;
+            TransactionTitle = transactionTitle;
+            TransactionAmount = transactionAmount;
+            Date = date;
+
+            TransactionNumberSeed++; //next transaction gets a different id
+        }
+    }
+}
